5fab365f6ce5c4e368d8564dc51643a298789fce
“WineWang0000” <1205294873@qq.com>
2019-06-01 23:06:01 +0800

    解决跨域，添加 :key

--- 用表单上传文件/index.js
+++ 用表单上传文件/index.js
@@ -10,7 +10,18 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/upload', upload.single('xxx'), (req,res)=>{
-	console.log(req.file)
-	res.send('看我')
+	res.set( 'Access-Control-Allow-Origin', 'http://null.jsbin.com')
+	res.send(req.file.filename)
+})
+app.get('preview/:key', (req, res)=>{
+	res.sendFile(`yyy/${req.params.key}`,{
+		root: __dirname,
+		headers:{
+			'Content-Type':'image/jepg'
+		},
+	},(error)=>{
+		res.status(404).send('file not found')
+		console.log(error)
+  })
 })
 app.listen(3000)
